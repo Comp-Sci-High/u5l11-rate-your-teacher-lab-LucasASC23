d1323bf8578a6e1df03d7d4e626520fcb5766370
LucasASC23 <139932248+LucasASC23@users.noreply.github.com>
2026-04-17 19:18:55 +0000

    Lab

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const express = require("express");
 
+const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
